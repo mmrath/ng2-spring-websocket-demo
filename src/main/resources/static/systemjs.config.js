86e94d5373c7d8c09f6e6fc236ca08ca0830605b
Remi <riseremi@icloud.com>
2016-08-31 18:25:13 +0300

    Replace npmcdn with unpkg

--- src/main/resources/static/systemjs.config.js
+++ src/main/resources/static/systemjs.config.js
@@ -11,10 +11,10 @@
   var map = {
     'app':                        'app',
 
-    '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
-    'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
-    'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
-    'typescript':                 'https://npmcdn.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
+    '@angular':                   'https://unpkg.com/@angular', // sufficient if we didn't pin the version
+    'rxjs':                       'https://unpkg.com/rxjs@5.0.0-beta.6',
+    'ts':                         'https://unpkg.com/plugin-typescript@4.0.10/lib/plugin.js',
+    'typescript':                 'https://unpkg.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
  };
 
   //packages tells the System loader how to load when no filename and/or no extension
@@ -34,7 +34,7 @@
   // Add map entries for each angular package
   // only because we're pinning the version with `ngVer`.
   ngPackageNames.forEach(function(pkgName) {
-    map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+    map['@angular/'+pkgName] = 'https://unpkg.com/@angular/' + pkgName + ngVer;
   });
 
   // Add package entries for angular packages
