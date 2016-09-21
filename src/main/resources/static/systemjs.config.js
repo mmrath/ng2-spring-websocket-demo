8b297de7644da89284fdbb546a6837482a2175a2
Murali Mohan Rath <mmrath@gmail.com>
2016-09-21 21:32:36 +1000

    Upgrade to angular 2 final

--- src/main/resources/static/systemjs.config.js
+++ src/main/resources/static/systemjs.config.js
@@ -5,14 +5,14 @@
  */
 (function(global) {
 
-  var ngVer = '@2.0.0-rc.6'; // lock in the angular package version; do not let it float to current!
+  var ngVer = '@2.0.0'; // lock in the angular package version; do not let it float to current!
 
   //map tells the System loader where to look for things
   var map = {
     'app':                        'app',
 
     '@angular':                   'https://unpkg.com/@angular', // sufficient if we didn't pin the version
-    'rxjs':                       'https://unpkg.com/rxjs@5.0.0-beta.11',
+    'rxjs':                       'https://unpkg.com/rxjs@5.0.0-beta.12',
     'ts':                         'https://unpkg.com/plugin-typescript@4.0.10/lib/plugin.js',
     'typescript':                 'https://unpkg.com/typescript@2.0.2/lib/typescript.js',
  };
